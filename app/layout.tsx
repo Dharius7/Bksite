ede1a078edf3b4e8bcd7790e3ccb15f6a59843f0
Dharius7 <azametthew@gmail.com>
2026-02-16 16:09:47 +0000

    Third Update commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,7 @@ import ThemeProvider from "@/components/ThemeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Orine Credit Bank LTD - Member-Focused Banking",
-  description: "Building financial strength together with personalized banking solutions for every member.",
-};
+export const metadata: Metadata = {`r`n  title: "Orine Credit",`r`n  description: "Building financial strength together with personalized banking solutions for every member.",`r`n  icons: {`r`n    icon: "/images/Logo.png",`r`n    shortcut: "/images/Logo.png",`r`n    apple: "/images/Logo.png",`r`n  },`r`n};
 
 export default function RootLayout({
   children,
